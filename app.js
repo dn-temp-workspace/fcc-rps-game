26ff1e55976a3bd7e671fe8d94afd9ebb22f48cf
Daniel Nowak <dnuxtools@gmail.com>
2018-11-13 23:00:57 +0100

    scripting application

---
+++ app.js
@@ -0,0 +1,67 @@
+let userScore = 0;
+let computerScore = 0;
+let userScore_span = document.getElementById("user-score");
+let computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+function getComputerChoice() {
+    const choices = ['r', 'p', 's'];
+    const randomNumber = (Math.floor(Math.random() * 3));
+    return choices[randomNumber];
+}
+
+function win(userChoice, computerChoice) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span = computerScore;
+    result_div.innerHTML = userChoice + " beats " + computerChoice;
+}
+
+function lose() {
+    console.log("COM wins");
+}
+
+function draw() {
+    console.log("It's a draw");
+}
+
+function game(userChoice) {
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            win(userChoice, computerChoice);
+            break;
+        case "rp":
+        case "ps":
+        case "sr":
+            lose();
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            draw();
+            break;
+    }
+}
+
+function main() {
+    rock_div.addEventListener("click", function () {
+        game("r");
+    })
+
+    paper_div.addEventListener("click", function () {
+        game("p");
+    })
+
+    scissors_div.addEventListener("click", function () {
+        game("s");
+    })
+}
+
+main();
